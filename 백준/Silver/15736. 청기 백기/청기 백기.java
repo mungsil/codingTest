4f9b2254bb2aa2b2b35af83c0ffa78e23d7bda09
mungsil <thddmd2009@gmail.com>
2025-04-26 23:56:30 +0900

    [Silver IV] Title: 청기 백기, Time: 176 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        System.out.println((int)Math.sqrt(input));
+    }
+}
