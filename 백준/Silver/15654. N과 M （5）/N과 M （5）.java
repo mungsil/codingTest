e75d49bd86cc9a1770752c53eca4983c3f165094
mungsil <thddmd2009@gmail.com>
2025-05-15 00:04:06 +0900

    [Silver III] Title: N과 M (5), Time: 1976 ms, Memory: 84988 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+/*
+4 2
+9 8 7 1
+*/
+
+public class Main{
+    
+    private static int max;
+    private static int num;
+    private static int[] inputArray;
+    private static Deque<Integer> buffer = new ArrayDeque<>();
+    private static Map<Integer,Boolean> checker = new HashMap<>();
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        max = scan.nextInt();
+        num = scan.nextInt();
+  
+        inputArray = new int[max];
+        for(int i=0; i<max; i++){
+            inputArray[i] = scan.nextInt();
+        }
+            
+        Arrays.sort(inputArray);
+        recur(0);
+    }
+    
+    public static void recur(int step){
+        if(step == num){
+            // 출력
+            for(int b:buffer){
+                System.out.print(b+" ");
+            }
+            return;
+        }
+        
+        for(int i=0;i<max;i++){
+            if(checker.getOrDefault(inputArray[i], Boolean.FALSE)== true){
+                continue;
+            }
+            buffer.offerLast(inputArray[i]);
+            checker.put(inputArray[i], Boolean.TRUE);
+            recur(step+1);
+            buffer.pollLast();
+            checker.put(inputArray[i], Boolean.FALSE);
+        }
+    }
+    
+}
