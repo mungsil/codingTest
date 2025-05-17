a923aebc67f97243202f04018c8fff44be6773d5
mungsil <thddmd2009@gmail.com>
2025-05-17 11:18:45 +0900

    [Silver III] Title: 퇴사, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,17 +1,5 @@
 import java.util.*;
-/*
-10
-1 1
-1 2
-1 3
-1 4
-1 5
-1 6
-1 7
-1 8
-1 9
-1 10
-*/
+
 public class Main{
     private static int workableDay;
     private static int[][] days;
@@ -21,30 +9,26 @@ public class Main{
         Scanner scan = new Scanner(System.in);
         workableDay = scan.nextInt();
         
-        days = new int[workableDay][2];
+        days = new int[20][2];
         for(int i = 0; i<workableDay; i++){
             days[i][0] = scan.nextInt();
             days[i][1] = scan.nextInt();
         }
         
-        recur(0,0,0);
+        recur(0,0);
         System.out.print(maxPrice);
     }
     
-    private static void recur(int step, int price, int watingDay){
+    private static void recur(int step, int price){
+        if(step>workableDay){
+            return;
+        }
         if(step==workableDay){
             maxPrice = Math.max(maxPrice, price);
             return;
         }
         
-        if(watingDay > 0){
-            recur(step+1, price, watingDay-1);
-        }else if( days[step][0] + step <= workableDay){
-            recur(step+1, price + days[step][1], days[step][0]-1);
-            recur(step+1, price, watingDay);
-        }else{
-            recur(step+1, price, watingDay);
-        }
-        
+        recur(step + days[step][0], price+days[step][1]);
+        recur(step+1, price);
     }
 }
