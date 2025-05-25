a4049c3a12c6c10ad7aef507b78ac6c5c3fed4a3
mungsil <thddmd2009@gmail.com>
2025-05-25 12:53:01 +0900

    [Gold III] Title: 내리막 길, Time: 1084 ms, Memory: 211024 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+/*
+4 5
+50 45 37 32 30
+35 50 40 20 25
+30 30 25 17 28
+27 24 22 15 10
+*/
+
+public class Main{
+    
+    private static int m; // 세로
+    private static int n; // 가로
+    private static int[][] map;
+    private static int[][] dp;
+    private static int[][] visited;
+    private static int[][] upDownLeftRight = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    
+    public static void main(String[] args){
+        // 입력 
+        Scanner scan = new Scanner(System.in);
+        
+        m = scan.nextInt();
+        n = scan.nextInt();
+        map = new int[m][n];
+        dp = new int[m][n];
+        visited = new int[m][n];
+        
+        for(int y=0; y<m; y++){
+            for(int x=0; x<n; x++){
+                map[y][x] = scan.nextInt();
+            }
+        }
+        
+        // 계산
+        System.out.print(recur(0,0));
+        
+    }
+    
+    private static int recur(int y, int x){
+        if(x==n-1 && y==m-1){
+            return 1;
+        }
+        
+        if(dp[y][x] != 0){
+            return dp[y][x];
+        }
+        
+        if(visited[y][x] != 0){
+            return dp[y][x];
+        }
+        
+        // 방문했어도 0인 경우
+        
+        for(int[] nextChange: upDownLeftRight){
+            int nextX = nextChange[0] + x;
+            int nextY = nextChange[1] + y;
+            
+            // 지도 범위 안
+            if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < m){
+                // 내리막길
+                if(map[y][x] > map[nextY][nextX]){
+                    dp[y][x] += recur(nextY, nextX);
+                    visited[y][x] =1;
+                }
+            }
+        }
+        
+        return dp[y][x];
+    }
+     
+}
