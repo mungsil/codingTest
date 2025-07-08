df7c10cabf50c6d24e2ecb6a95e1d89080182359
mungsil <thddmd2009@gmail.com>
2025-07-08 11:20:13 +0900

    [level 1] Title: 폰켓몬, Time: 5.37 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer;
         int selectableNum = nums.length/2;
         
         Set<Integer> ponketmoneSortSet = new HashSet<>();
         for(int n: nums){
             ponketmoneSortSet.add(n);
         }
+        
         int maxSort = ponketmoneSortSet.size();
-        
         if(maxSort >= selectableNum){
-            answer = selectableNum;
+            return selectableNum;
         } else{
-            answer = maxSort;
+            return maxSort;
         }
-        
-        return answer;
     }
 }
