2368c2676a1538256275759017ccec0230237cd7
mungsil <thddmd2009@gmail.com>
2025-07-09 12:05:33 +0900

    [level 2] Title: 의상, Time: 0.53 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,20 +4,19 @@ class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> clothesCountMap = new HashMap<>();
         for(String[] c: clothes){
-            String sort = c[0];
-            String name = c[1];
+            String name = c[0];
+            String sort = c[1];
             
             clothesCountMap.merge(
-                name,
+                sort,
                 1,
                 (oldValue, newValue) -> oldValue+newValue
             );
         }
         
         int answer = 1;
-        for(String key: clothesCountMap.keySet()){
-            int count = clothesCountMap.get(key) + 1;
-            answer *= count;
+        for(int counts: clothesCountMap.values()){
+            answer *= counts + 1;
         }
         
         return answer-1;
