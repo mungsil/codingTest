1321cf2b126ef1502cb71db8afce42f0fc30ed68
mungsil <thddmd2009@gmail.com>
2025-08-08 23:53:36 +0900

    [Silver II] Title: 스택 수열, Time: 816 ms, Memory: 106196 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -18,30 +18,25 @@ public class Main{
         
         Deque<Integer> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
-        int currentNum = 1;
-        stack.push(currentNum);
-        sb.append('+').append('\n');   
+        int current = 1;
+        stack.push(current);
+        sb.append('+').append('\n');    
 
         for(int i = 0; i < num; i++){
-            int out = scan.nextInt();
-            if(stack.isEmpty()){
-                stack.push(++currentNum);
-                sb.append('+').append('\n');  
-            }
-            
-            if(out < stack.peek()){
-               System.out.print("NO");
-               return;
-            }
-
-            while(out > stack.peek()){
-                currentNum++; 
-                stack.push(currentNum); 
+            int target = scan.nextInt();
+            while(target > current){
+                current++; 
+                stack.push(current); 
                 sb.append('+').append('\n');                    
             }
             
-            stack.pop();
-            sb.append('-').append('\n');
+            Integer top = stack.pop();
+            if(top != null && top == target){
+                sb.append('-').append('\n');
+            } else{
+                System.out.print("NO");
+                return;
+            } 
         }
         
         System.out.print(sb);
