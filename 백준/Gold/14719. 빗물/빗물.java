4a44acc4954167904283b517f1fb7624b1d57eef
mungsil <thddmd2009@gmail.com>
2025-08-19 00:17:23 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,43 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int H = Integer.parseInt(input[0]);
-        int W = Integer.parseInt(input[1]);
-        
+        int[] size = Arrays.stream(br.readLine().split(" "))
+                           .mapToInt(Integer::parseInt)
+                           .toArray();
+        int num = size[1];
+  
         int[] blocks = Arrays.stream(br.readLine().split(" "))
                            .mapToInt(Integer::parseInt)
                            .toArray();
-        
+
         int answer = 0;
-        
-        // 각 위치에서 왼쪽 최대 높이와 오른쪽 최대 높이를 미리 계산
-        int[] leftMax = new int[W];
-        int[] rightMax = new int[W];
-        
-        // 왼쪽 최대 높이 계산
-        leftMax[0] = blocks[0];
-        for (int i = 1; i < W; i++) {
-            leftMax[i] = Math.max(leftMax[i-1], blocks[i]);
-        }
-        
-        // 오른쪽 최대 높이 계산
-        rightMax[W-1] = blocks[W-1];
-        for (int i = W-2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i+1], blocks[i]);
-        }
-        
-        // 각 위치에서 고일 수 있는 물의 양 계산
-        for (int i = 0; i < W; i++) {
-            int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            if (waterLevel > blocks[i]) {
-                answer += waterLevel - blocks[i];
+        int leftP = 0;
+        int leftMaxH = blocks[leftP];
+        int rightP = num-1;
+        int rightMaxH = blocks[rightP];
+ 
+        while(leftP < rightP){
+            leftMaxH = Math.max(leftMaxH, blocks[leftP]);
+            rightMaxH = Math.max(rightMaxH, blocks[rightP]);
+            
+            if(leftMaxH <= rightMaxH){
+                answer += leftMaxH - blocks[leftP];
+                leftP++;
+            } else{
+                answer += rightMaxH - blocks[rightP];
+                rightP--;
             }
         }
         
         System.out.println(answer);
     }
+    
 }
