b162bf616b81f866a44e278306dd31c2d77ecb45
mungsil <thddmd2009@gmail.com>
2026-01-01 18:38:36 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 176 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main{
+    
+    static int max;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        max = sc.nextInt();
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+
+        Main main = new Main();
+        main.solve(0);
+        System.out.println(sb.toString());
+    }
+
+    private void solve(int num){ // 0, 1, 2
+        StringBuilder underBarBuilder = new StringBuilder(); 
+        for(int i=0; i<num; i++){
+            underBarBuilder.append("____");
+        }
+
+        sb.append(underBarBuilder.toString()).append("\"재귀함수가 뭔가요?\"").append("\n");
+        if(num == max){ // 0 != 2
+            sb.append(underBarBuilder.toString())
+                    .append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+        } else{
+            sb.append(underBarBuilder.toString())
+                    .append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n")
+                    .append(underBarBuilder.toString())
+                    .append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n")
+                    .append(underBarBuilder.toString())
+                    .append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+
+            solve(num+1);
+        }
+
+        sb.append(underBarBuilder.toString())
+                .append("라고 답변하였지.\n");
+    }
+
+    
+}
