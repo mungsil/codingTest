11b6b0be5541e0bb7b9082b14e15d755daa608ed
mungsil <thddmd2009@gmail.com>
2026-02-06 23:57:16 +0900

    [Gold V] Title: 톱니바퀴, Time: 92 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,95 +1,74 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Main{
     
-    static int[][] wheels = new int[4][8];
+    static int[][] gear = new int[4][8];
+    static int[] d; // 각 톱니바퀴 회전 방향
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        for(int i=0; i<4; i++){
-            wheels[i] = readWheel(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                gear[i][j] = line.charAt(j) - '0';
+            }
         }
-
-        int k = Integer.parseInt(br.readLine());
         
-        for(int i=0; i<k; i++){
-            rotate(br.readLine());
+        int K = Integer.parseInt(br.readLine()); // 회전 횟수
+        while(K-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken()) - 1; // 바퀴 번호 (0~3)
+            int dir = Integer.parseInt(st.nextToken()); // 방향 (1: 시계, -1: 반시계)
+            
+            d = new int[4];
+            d[idx] = dir;
+            
+            checkLeft(idx);
+            checkRight(idx);
+            
+            for(int i=0; i<4; i++){
+                if(d[i]!=0)rotate(i, d[i]);
+            }
         }
+        
         int score = 0;
         for(int i=0; i<4; i++){
-            int result = wheels[i][0];
-            if(result == 1){ // S
-                score+= (int) Math.pow(2, i);
+            if(gear[i][0] == 1) {
+                score+=Math.pow(2,i);
             }
         }
-        System.out.print(score);
+        System.out.println(score);
     }
     
-    private static void rotate(String input){
-        // 회전시킬 톱니바퀴 번호
-        String[] parts = input.split(" ");
-        int wheelNum = Integer.parseInt(parts[0])-1;
-        // 회전 방향 {1:시계}, {-1:반시계}
-        int direct = Integer.parseInt(parts[1]);
-        
-        // === 풀이
-        boolean[] rotated = new boolean[4]; // 바퀴 회전 여부
-        
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{wheelNum, direct});
-        while(!queue.isEmpty()){
-            int[] curr = queue.poll();
-            int cn = curr[0];
-            int cd = curr[1];
-            rotated[cn] = true;
-            
-            int[] wheel = wheels[cn];
-            for(int dx: new int[]{-1, 1}){
-                int nextWheelNum = cn+dx;
-                if(nextWheelNum >= 0 && nextWheelNum <4 &&
-                    rotated[nextWheelNum] == false
-                ){
-                    int[] nWheel = wheels[nextWheelNum];
-                    if(dx == 1){ // 오른쪽
-                        if(wheel[2] != nWheel[2+4]){
-                            queue.add(new int[]{nextWheelNum, cd == 1 ? -1 : 1});
-                        }                        
-                    } else {
-                        if(wheel[2+4] != nWheel[2]){
-                            queue.add(new int[]{nextWheelNum, cd == 1 ? -1 : 1});
-                        }                        
-                    }                    
-
-                }
-            }
-
-            int wheelLength = 8;
-            // 시계 방향 회전
-            // 101
-            int[] rotatedWheel = new int[8];
-            if(cd==1){
-                for(int i=0; i<wheelLength; i++){
-                    int toIdx = (i+1) % wheelLength; 
-                    rotatedWheel[toIdx] = wheel[i];
-                }                
-            } else{ // 반시계 방향 회전
-               for(int i=0; i<wheelLength; i++){
-                    int toIdx = (i+7) % wheelLength; 
-                    rotatedWheel[toIdx] = wheel[i];
-                } 
-            }
-            
-            wheels[cn] = rotatedWheel;
+    static void checkLeft(int idx){
+        if(idx > 0 && gear[idx][6] != gear[idx-1][2]){
+            d[idx-1] = -d[idx];
+            checkLeft(idx-1);
         }
     }
     
-    private static int[] readWheel(String input){
-        int[] arr = new int[8];
-        for(int i=0; i<8; i++){
-            arr[i] = input.charAt(i)-'0';
+    static void checkRight(int idx){
+        if(idx < 3 && gear[idx][2] != gear[idx+1][6]){
+            d[idx+1] = -d[idx];
+            checkRight(idx+1);
         }
-        return arr;
     }
- }
+    
+    static void rotate(int idx, int dir){
+        if(dir == 1){ // 시계
+            int temp = gear[idx][7];
+            for(int i=7; i>0; i--){
+                gear[idx][i] = gear[idx][i-1];
+            }
+            gear[idx][0] = temp;
+        } else{ // 반시계
+            int temp = gear[idx][0];
+            for(int i=0; i<7; i++){
+                gear[idx][i] = gear[idx][i+1];
+            }
+            gear[idx][7] = temp;
+        }
+    }
+    
+}
